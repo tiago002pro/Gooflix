8db5954a04c47a5f5f73330733c436d352448b2e
Tiago Barbosa <tiagobarbosa02@outlook.com>
2020-07-28 04:56:35 -0300

    Setup Inicial do Projeto

---
+++ src/components/Menu/components/ButtonLink/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ButtonLink() {
+    return (
+        <a href="/" className="VoceSabeOQueTemLa">
+            Novo Vídeo
+        </a>
+    );
+}
+
+export default ButtonLink;
